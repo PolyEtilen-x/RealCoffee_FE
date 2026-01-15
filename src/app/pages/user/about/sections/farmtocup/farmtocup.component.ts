2078e76646b21b13285261180a1d5ff2fd7cbd9c
PolyEtilen-x <nguyenhongtonpy2004@gmail.com>
2026-01-15 23:52:05 +0700

    fix

--- src/app/pages/user/about/sections/farmtocup/farmtocup.component.ts
+++ src/app/pages/user/about/sections/farmtocup/farmtocup.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RevealDirective } from '../../../../../shared/directives/reveal.directive';
 
 @Component({
   selector: 'app-farm-to-cup',
   standalone: true,
+  imports: [RevealDirective],
   templateUrl: './farmtocup.component.html',
   styleUrl: './farmtocup.component.css'
 })
--- src/app/pages/user/about/sections/quality/quality.component.ts
+++ src/app/pages/user/about/sections/quality/quality.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RevealDirective } from '../../../../../shared/directives/reveal.directive';
 
 @Component({
   selector: 'app-quality',
   standalone: true,
+  imports: [ RevealDirective ],
   templateUrl: './quality.component.html',
   styleUrl: './quality.component.css'
 })
