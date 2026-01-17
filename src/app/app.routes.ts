5fb88964bc203f00db866183cb3388c539013e3f
PolyEtilen-x <nguyenhongtonpy2004@gmail.com>
2026-01-18 00:03:53 +0700

    nav

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,16 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/user/home/home.component';
 import { AboutComponent } from './pages/user/about/about.component';
+import { LoginComponent } from './pages/auth/login/login.component';
+import { RegisterComponent } from './pages/auth/register/register.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'about', component: AboutComponent}           
+  { path: 'about', component: AboutComponent},
+  { path: 'login', component: LoginComponent},
+  { path: 'register', component: RegisterComponent },
+
+  //default
+  { path: '', redirectTo: '/', pathMatch: 'full' }
+
 ];
